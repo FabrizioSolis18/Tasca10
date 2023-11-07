1a26386ddccec62386646f1713048c1286285f34
FabrizioSolis18 <148865197+FabrizioSolis18@users.noreply.github.com>
2023-11-07 14:01:04 +0100

    Add files via upload

---
+++ Ex11.py
@@ -0,0 +1,38 @@
+#Exercici 11. Calculadora
+def menu_principal ():
+    print
+    Menu principal: 
+    1. Calculadora de números enters
+    2. Calculadora de numeros reals
+    3. Sortir
+
+"")
+
+    a = int (imput("Elegeix una opció: "))
+    return a
+    def calculadora enters()
+     :print("Hem passat per la calculadora d'enters")
+
+     def calculadora reals')
+
+#Programa principal
+
+op = 1
+case op= -1
+print ("")
+       1. Sumar
+       2. Restar
+       3. Multripicar
+       4. Dividir
+       5. Sortir
+     ("")
+op int (input("Elegeix una opció"))
+match op:
+    case 1: #Sumar
+        x = int(input("Introdueix el primer número"))
+        y = int(input("Introdueix el segon número"))
+        print("() + () = ()", format(x,y,x+y))
+    case 2: #Restar
+        x = int (input ("Introdueix el primer número: "))
+        y = int (input ("Introdueix el segon número: ")) 
+        print ("")
